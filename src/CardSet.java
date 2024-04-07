1280d439fa625dc1326c7972f019b905f6ae2105
MeBrianna <emojibrianna9@gmail.com>
2024-04-07 09:52:01 -0700

    added card images to CardSet

--- src/CardSet.java
+++ src/CardSet.java
@@ -8,74 +8,77 @@ public class CardSet {
 	public CardSet(String category) {
 		this.category = category;
 		cards = new ArrayList<>();
-		// TODO: update category names
-		if (category.equals("XXX")) {
-			setCategory1Cards();
-		} else if (category.equals("XXX")) {
-			setCategory2Cards();
-		} else if (category.equals("XXX")) {
-			setCategory3Cards();
-		} else if (category.equals("XXX")) {
-			setCategory4Cards();
-		} else if (category.equals("XXX")) {
-			setCategory5Cards();
+		if (category.equals("cacti")) {
+			setCactiCards();
+		} else if (category.equals("cities")) {
+			setCitiesCards();
+		} else if (category.equals("mammals")) {
+			setMammalsCards();
+		} else if (category.equals("mountains")) {
+			setMountainsCards();
+		} else if (category.equals("reptiles")) {
+			setReptilesCards();
 		}
 	}
-	
-	// TODO: update card names and their file names
-	private void setCategory1Cards() {
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
+
+	private void setCactiCards() {
+		String basePath = "file:src/images/cards/cacti/";
+		cards.add(new Card("apache hedgehog cactus", basePath + "cacti_apachehedgehogcactus.png"));
+		cards.add(new Card("barrel cactus", basePath + "cacti_barrelcactus.png"));
+		cards.add(new Card("jumping cholla", basePath + "cacti_jumpingcholla.png"));
+		cards.add(new Card("organ cactus", basePath + "cacti_organcactus.png"));
+		cards.add(new Card("pincushion cactus", basePath + "cacti_pincushioncactus.png"));
+		cards.add(new Card("prickly pear", basePath + "cacti_pricklypear.png"));
+		cards.add(new Card("saguaro", basePath + "cacti_saguaro.png"));
+		cards.add(new Card("teddy bear cholla", basePath + "cacti_teddybearcholla.png"));
 	}
 	
-	private void setCategory2Cards() {
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
+	private void setCitiesCards() {
+		String basePath = "file:src/images/cards/cities/";
+		cards.add(new Card("Bisbee", basePath + "cities_bisbee.png"));
+		cards.add(new Card("Flagstaff", basePath + "cities_flagstaff.png"));
+		cards.add(new Card("Jerome", basePath + "cities_jerome.png"));
+		cards.add(new Card("Pheonix", basePath + "cities_phoenix.png"));
+		cards.add(new Card("Scottsdale", basePath + "cities_scottsdale.png"));
+		cards.add(new Card("Sedona", basePath + "cities_sedona.png"));
+		cards.add(new Card("Tombstone", basePath + "cities_tombstone.png"));
+		cards.add(new Card("Tucson", basePath + "cities_tucson.png"));
 	}
 	
-	private void setCategory3Cards() {
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
+	private void setMammalsCards() {
+		String basePath = "file:src/images/cards/mammals/";
+		cards.add(new Card("antelope jackrabbit", basePath + "mammals_antelopejackrabbit.png"));
+		cards.add(new Card("black bear", basePath + "mammals_blackbear.png"));
+		cards.add(new Card("coyote", basePath + "mammals_coyote.png"));
+		cards.add(new Card("javelina", basePath + "mammals_javelina.png"));
+		cards.add(new Card("lesser long-nosed bat", basePath + "mammals_lesserlongnosedbat.png"));
+		cards.add(new Card("mountain lion", basePath + "mammals_mountainlion.png"));
+		cards.add(new Card("mule deer", basePath + "mammals_muledeer.png"));
+		cards.add(new Card("white-nosed coati", basePath + "mammals_whitenosedcoati.png"));
 	}
 	
-	private void setCategory4Cards() {
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
+	private void setMountainsCards() {
+		String basePath = "file:src/images/cards/mountains/";
+		cards.add(new Card("A Mountain", basePath + "mountains_amountain.png"));
+		cards.add(new Card("Mt Lemmon", basePath + "mountains_mtlemmon.png"));
+		cards.add(new Card("Quinlan Mountains", basePath + "mountains_quinlanmountains.png"));
+		cards.add(new Card("Rincon Mountains", basePath + "mountains_rinconmountains.png"));
+		cards.add(new Card("Santa Catalina Mountains", basePath + "mountains_santacatalinamountains.png"));
+		cards.add(new Card("Santa Rita Mountains", basePath + "mountains_santaritamountains.png"));
+		cards.add(new Card("Tortolita Mountains", basePath + "mountains_tortolitamountains.png"));
+		cards.add(new Card("Tucson Mountains", basePath + "mountains_tucsonmountains.png"));
 	}
 	
-	private void setCategory5Cards() {
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
-		cards.add(new Card("", ""));
+	private void setReptilesCards() {
+		String basePath = "file:src/images/cards/reptiles/";
+		cards.add(new Card("desert iguana", basePath + "reptiles_desertiguana.png"));
+		cards.add(new Card("gila monster", basePath + "reptiles_gilamonster.png"));
+		cards.add(new Card("greater short-horned lizard", basePath + "reptiles_greatershorthornedlizard.png"));
+		cards.add(new Card("ornate tree lizard", basePath + "reptiles_ornatetreelizard.png"));
+		cards.add(new Card("regal horned lizard", basePath + "reptiles_regalhornedlizard.png"));
+		cards.add(new Card("sidewinder", basePath + "reptiles_sidewinder.png"));
+		cards.add(new Card("sonoran collared lizard", basePath + "reptiles_sonorancollaredlizard.png"));
+		cards.add(new Card("western diamondback rattlesnake", basePath + "reptiles_westerndiamondbackrattlesnake.png"));
 	}
 	
 	public ArrayList<Card> getCards() {
---
+++ src/TestTable.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class TestTable extends Application {
+	
+	public static void main(String args[]) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+	    GridPane pane = new GridPane();
+	    // change first parameter to change category
+	    Table table = new Table("cacti", 4);
+	    Card[] cards = table.getRandomizedCards();
+	    
+	    // adds randomized card images to grid pane
+	    for (int i = 0; i < 8; i ++) {
+	    	ImageView imageView = new ImageView(new Image(cards[i].getFileName()));
+	    	imageView.setFitHeight(200);
+		    imageView.setFitWidth(125);
+		    if (i < 4) {
+		    	pane.add(imageView, i % 4, 0);
+		    } else {
+		    	pane.add(imageView, i % 4, 1);
+		    }
+	    }
+	    
+	    Scene scene = new Scene(pane, 500, 400);
+	    stage.setScene(scene);
+	    stage.show();
+	
+	}
+
+}
