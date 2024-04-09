886babb0df2bc05484d858883d5dc39fa42a063b
MeBrianna <emojibrianna9@gmail.com>
2024-04-09 15:47:33 -0700

    added instance variable

--- src/Card.java
+++ src/Card.java
@@ -3,6 +3,7 @@ public class Card {
 	
 	private String name;
 	private String fileName;
+	private boolean hasBeenCorrectlyGuessed;
 	
 	public Card(String name, String fileName) {
 		this.name = name;
@@ -15,6 +16,14 @@ public class Card {
 	
 	public String getFileName() {
 		return fileName;
+	}
+	
+	public void setCorrectlyGuessed() {
+		hasBeenCorrectlyGuessed = true;
+	}
+	
+	public boolean getHasBeenCorrectlyGuessed() {
+		return hasBeenCorrectlyGuessed;
 	}
 	
 }
