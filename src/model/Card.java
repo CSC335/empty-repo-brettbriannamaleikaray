1758ecf4434b19228b82fba69342f6ebadafb645
brettbrimmer <bbrimmer@arizona.edu>
2024-04-18 03:06:50 -0700

    Fixed click coordinates in RoundPane, made cards go away when matched

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,25 +1,54 @@
 package model;
 
 /**
- * represents a card and contains the name of the card and the file name
- * of the card image
+ * represents a card and contains the name of the card and the file name of the
+ * card image
  */
 public class Card {
-	
+
 	private String name;
 	private String fileName;
-	
+	private boolean flipped;
+	private boolean destroyed;
+
 	/**
 	 * creates a card with the given name and file name
 	 * 
-	 * @param name  the name of the card
-	 * @param fileName  the file name where the card image is located
+	 * @param name     the name of the card
+	 * @param fileName the file name where the card image is located
 	 */
 	public Card(String name, String fileName) {
 		this.name = name;
 		this.fileName = fileName;
 	}
-	
+
+	/**
+	 * Let's us know if if the card is flipped face up or not
+	 * 
+	 * @return A boolean. "True" means card is flipped face up, "False" means it's
+	 *         face down
+	 */
+	public boolean isFlipped() {
+		return flipped;
+	}
+
+	/**
+	 * Let's us know if if the card has been removed from the board
+	 * 
+	 * @return A boolean. "True" means card has been removed from the board
+	 */
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+
+	/**
+	 * Changes the card's 'destroyed' status (meaning it's been removed from the
+	 * board) to true
+	 */
+	public void destroyCard() {
+		destroyed = true;
+	}
+
 	/**
 	 * gets the card name
 	 * 
@@ -28,7 +57,7 @@ public class Card {
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * gets the file name
 	 * 
@@ -37,5 +66,15 @@ public class Card {
 	public String getFileName() {
 		return fileName;
 	}
-	
+
+	/**
+	 * Changes the card's flipped flag (which indicates whether the card is flipped)
+	 * to the opposite value
+	 */
+	public void flipCard() {
+		if (flipped == true)
+			flipped = false;
+		else
+			flipped = true;
+	}
 }
